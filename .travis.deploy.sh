08dcbbcc107543954957c626f0350df5334b4543
Aliaksei Syrel <alex.syrel@gmail.com>
2019-04-08 20:14:58 +0200

    Update deploy script

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -5,17 +5,19 @@ then
 else
 	travis_fold start "Deploy"
 	
-	cd 
+	ls -la
+	
+	cd release
 	
 	if [[ $TRAVIS_OS_NAME == "osx" ]]; then
-		scp libGlutin.so $FEENK_CLOUD:/var/www/html/Glutin/development/x86_64
+		scp libGlutin.dylib $FEENK_CLOUD:/var/www/html/Glutin/osx/development/x86_64
 	fi
 	if [[ $TRAVIS_OS_NAME == "linux" ]]; then
-		
+		scp libGlutin.so $FEENK_CLOUD:/var/www/html/Glutin/linux/development/x86_64
 	fi
 	
 	if [[ $TRAVIS_OS_NAME == "windows" ]]; then
-		
+		scp libGlutin.dll $FEENK_CLOUD:/var/www/html/Glutin/windows/development/x86_64
 	fi
 
 	travis_fold end "Deploy"
