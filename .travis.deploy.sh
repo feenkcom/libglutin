4e081d1f93597d8314caf195ed71e1e09303312b
Aliaksei Syrel <alex.syrel@gmail.com>
2019-04-08 20:38:01 +0200

    Move ssh key part to deployment script

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -2,17 +2,17 @@
 if [[ $TRAVIS_COMMIT_MESSAGE == *"[skip deploy]"* ]]
 then
 	echo "Skipping deploy stage"
-else	
-	ls -la
-	
-	cd target
-	
-	ls -la
-	
+else
+	echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+	openssl aes-256-cbc -K $encrypted_b4e233c452d3_key -iv $encrypted_b4e233c452d3_iv -in deploy_key.enc -out ./deploy_key -d
+	eval "$(ssh-agent -s)"
+	chmod 600 ./deploy_key
+	ssh-add ./deploy_key
+	ssh -i ./deploy_key $FEENK_CLOUD pwd
+
+	cd target/release
 	cd release
 
-	ls -la
-	
 	if [[ $TRAVIS_OS_NAME == "osx" ]]; then
 		scp libGlutin.dylib $FEENK_CLOUD:/var/www/html/Glutin/osx/development/x86_64
 	fi
