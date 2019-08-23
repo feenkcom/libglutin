ab387c3a28c5d77ef7263662788a9356e3d6b203
Aliaksei Syrel <alex.syrel@gmail.com>
2019-08-23 13:31:29 +0200

    Fix warning related to _with_full_size

--- src/platform/macos.rs
+++ src/platform/macos.rs
@@ -5,11 +5,11 @@ use super::*;
 use platform::macos::WindowBuilderExtMacOS;
 
 #[no_mangle]
-pub fn glutin_window_builder_with_full_size(_ptr_window_builder: *mut WindowBuilder, with_full_size: bool) -> *mut WindowBuilder {
+pub fn glutin_window_builder_with_full_size(_ptr_window_builder: *mut WindowBuilder, _with_full_size: bool) -> *mut WindowBuilder {
     CBox::with_window_builder(_ptr_window_builder, |builder| {
         builder
-            .with_titlebar_transparent(with_full_size)
-            .with_fullsize_content_view(with_full_size)
-            .with_title_hidden(with_full_size)
+            .with_titlebar_transparent(_with_full_size)
+            .with_fullsize_content_view(_with_full_size)
+            .with_title_hidden(_with_full_size)
     })
 }
--- src/platform/others.rs
+++ src/platform/others.rs
@@ -1,6 +1,6 @@
 use super::*;
 
 #[no_mangle]
-pub fn glutin_window_builder_with_full_size(_ptr_window_builder: *mut WindowBuilder, with_full_size: bool) -> *mut WindowBuilder {
+pub fn glutin_window_builder_with_full_size(_ptr_window_builder: *mut WindowBuilder, _with_full_size: bool) -> *mut WindowBuilder {
     return _ptr_window_builder;
 }
