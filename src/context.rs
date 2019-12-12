8057d458104160c80428b082046837f105e319ae
Aliaksei Syrel <alex.syrel@gmail.com>
2019-12-12 09:05:08 +0100

    refactor window_builder to use ValueBox

--- src/context.rs
+++ src/context.rs
@@ -1,5 +1,4 @@
 use boxer::boxes::{ValueBox, ValueBoxPointer};
-use boxer::CBox;
 use glutin::dpi::PhysicalSize;
 use glutin::event_loop::EventLoop;
 use glutin::window::WindowBuilder;
@@ -11,7 +10,7 @@ use ContextApi;
 #[no_mangle]
 pub fn glutin_create_windowed_context(
     _ptr_events_loop: *mut ValueBox<EventLoop<()>>,
-    _ptr_window_builder: *mut WindowBuilder,
+    mut _ptr_window_builder: *mut ValueBox<WindowBuilder>,
     mut _ptr_context_builder: *mut ValueBox<ContextBuilder<NotCurrent>>,
 ) -> *mut ValueBox<WindowedContext<PossiblyCurrent>> {
     if _ptr_events_loop.is_null() {
@@ -31,35 +30,30 @@ pub fn glutin_create_windowed_context(
 
     _ptr_events_loop.with_not_null_return(std::ptr::null_mut(), |event_loop| {
         _ptr_context_builder.with_value_consumed(|context_builder| {
-            // we consume both window_builder and context_builder
-            let window_builder = unsafe { *CBox::from_raw(_ptr_window_builder) };
+            _ptr_window_builder.with_value_consumed(|window_builder| {
+                if cfg!(debug_assertions) {
+                    println!("[Glutin] OpenGL Context: {:?}", context_builder);
+                    println!("[Glutin] Window attributes: {:?}", window_builder);
+                }
 
-            if cfg!(debug_assertions) {
-                println!("[Glutin] OpenGL Context: {:?}", context_builder);
-                println!(
-                    "[Glutin] Primary monitor: {:?}",
-                    event_loop.primary_monitor()
-                );
-                println!("[Glutin] Window attributes: {:?}", window_builder);
-            }
-
-            match context_builder.build_windowed(window_builder, event_loop) {
-                Ok(windowed_context) => match unsafe { windowed_context.make_current() } {
-                    Ok(windowed_context) => ValueBox::new(windowed_context).into_raw(),
+                match context_builder.build_windowed(window_builder, event_loop) {
+                    Ok(windowed_context) => match unsafe { windowed_context.make_current() } {
+                        Ok(windowed_context) => ValueBox::new(windowed_context).into_raw(),
+                        Err(err) => {
+                            if cfg!(debug_assertions) {
+                                println!("[Glutin] Could not create context {:?}", err);
+                            }
+                            std::ptr::null_mut()
+                        }
+                    },
                     Err(err) => {
                         if cfg!(debug_assertions) {
                             println!("[Glutin] Could not create context {:?}", err);
                         }
                         std::ptr::null_mut()
                     }
-                },
-                Err(err) => {
-                    if cfg!(debug_assertions) {
-                        println!("[Glutin] Could not create context {:?}", err);
-                    }
-                    std::ptr::null_mut()
                 }
-            }
+            })
         })
     })
 }
--- src/window_builder.rs
+++ src/window_builder.rs
@@ -1,58 +1,58 @@
-use boxer::CBox;
-use boxer::string::BoxerString;
+use boxer::string::{BoxerString, BoxerStringPointer};
 use glutin::window::WindowBuilder;
 use glutin::dpi::{LogicalSize};
+use boxer::boxes::{ValueBox, ValueBoxPointer};
 
 #[no_mangle]
-pub fn glutin_create_window_builder() -> *mut WindowBuilder {
-    CBox::into_raw(WindowBuilder::new())
+pub fn glutin_create_window_builder() -> *mut ValueBox<WindowBuilder> {
+    ValueBox::new(WindowBuilder::new()).into_raw()
 }
 
 #[no_mangle]
-pub fn glutin_destroy_window_builder(_ptr: *mut WindowBuilder) {
-    CBox::drop(_ptr);
+pub fn glutin_destroy_window_builder(_ptr: *mut ValueBox<WindowBuilder>) {
+    _ptr.drop();
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_title(_ptr_window_builder: *mut WindowBuilder, _ptr_boxer_string: *mut BoxerString) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| {
-        CBox::with_raw(_ptr_boxer_string, |string| {
-          builder.with_title(string.to_string())
+pub fn glutin_window_builder_with_title(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, _ptr_boxer_string: *mut BoxerString) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| {
+        _ptr_boxer_string.with(|string| {
+            builder.with_title(string.to_string())
         })
-    })
+    });
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_decorations(_ptr_window_builder: *mut WindowBuilder, with_decorations: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_decorations(with_decorations))
+pub fn glutin_window_builder_with_decorations(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_decorations: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_decorations(with_decorations))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_transparency(_ptr_window_builder: *mut WindowBuilder, with_transparency: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_transparent(with_transparency))
+pub fn glutin_window_builder_with_transparency(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_transparency: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_transparent(with_transparency))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_resizable(_ptr_window_builder: *mut WindowBuilder, with_resizable: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_resizable(with_resizable))
+pub fn glutin_window_builder_with_resizable(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_resizable: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_resizable(with_resizable))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_dimensions(_ptr_window_builder: *mut WindowBuilder, width: f64, height: f64) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_inner_size(LogicalSize::new(width, height)))
+pub fn glutin_window_builder_with_dimensions(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, width: f64, height: f64) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_inner_size(LogicalSize::new(width, height)))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_maximized(_ptr_window_builder: *mut WindowBuilder, with_maximized: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_maximized(with_maximized))
+pub fn glutin_window_builder_with_maximized(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_maximized: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_maximized(with_maximized))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_visibility(_ptr_window_builder: *mut WindowBuilder, with_visibility: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_visible(with_visibility))
+pub fn glutin_window_builder_with_visibility(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_visibility: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_visible(with_visibility))
 }
 
 #[no_mangle]
-pub fn glutin_window_builder_with_always_on_top(_ptr_window_builder: *mut WindowBuilder, with_always_on_top: bool) -> *mut WindowBuilder {
-    CBox::with_consumable_raw(_ptr_window_builder, |builder| builder.with_always_on_top(with_always_on_top))
+pub fn glutin_window_builder_with_always_on_top(mut _ptr_window_builder: *mut ValueBox<WindowBuilder>, with_always_on_top: bool) {
+    _ptr_window_builder.with_not_null_value_mutate_consumed(|builder| builder.with_always_on_top(with_always_on_top))
 }
