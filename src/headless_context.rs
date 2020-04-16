e533c55b1aeff97c10f6e4bde9838dcc36a3b8b4
Aliaksei Syrel <alex.syrel@gmail.com>
2020-04-16 10:27:37 +0200

    fix missing use

--- src/headless_context.rs
+++ src/headless_context.rs
@@ -15,8 +15,9 @@ fn build_context_surfaceless<T1: ContextCurrentState>(
     el: &EventLoop<()>,
 ) -> Result<Context<NotCurrent>, CreationError> {
     use glutin::platform::unix::HeadlessContextExt;
+    use glutin::platform::unix::EventLoopWindowTargetExtUnix;
     if el.is_x11() {
-        return Err(CreationError::NotSupported("Surfaceless context is not supported with X11".into_string()))
+        return Err(CreationError::NotSupported(String::from_str("Surfaceless context is not supported with X11")))
     }
     cb.build_surfaceless(el)
 }
